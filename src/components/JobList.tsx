88d244d72fa61a11411002296111b809688f517a
Anton <superantoxa12@gmail.com>
2024-10-14 22:44:27 +0200

    feat: search, filtering by source

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -10,7 +10,7 @@ import _ from "lodash";
 
 // import { useRouter } from 'next/navigation';
 
-const JOBS_PER_PAGE = 10;
+const JOBS_PER_PAGE = 15;
 
 interface JobListProps {
   jobs: Job[];
@@ -34,15 +34,32 @@ export default function JobList({ jobs }: JobListProps) {
       window.scrollTo(0, 0);
     }
   }, [currentPage]);
-  // const router = useRouter();
 
-  // const currentPage = 1;
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [sourceFilter, setSourceFilter] = useState<string>('');
+
   const startIndex = (currentPage - 1) * JOBS_PER_PAGE;
-  const currentJobs = jobs.slice(startIndex, startIndex + JOBS_PER_PAGE);
 
-  // const handleJobClick = (jobId: number) => {
-  //   router.push(`/jobs/${jobId}`);
-  // };
+  const filteredJobs = jobs.filter(job => {
+    const titleMatch = searchTerm === '' ? true : job.title?.toLowerCase().includes(searchTerm.toLowerCase())
+        || job.description?.toLowerCase().includes(searchTerm.toLowerCase())
+        || false;
+    const sourceMatch = sourceFilter === '' ? true : job.source?.toLowerCase().includes(sourceFilter.toLowerCase()) || false;
+
+    return titleMatch && sourceMatch;
+  });
+
+  const currentJobs = filteredJobs.slice(startIndex, startIndex + JOBS_PER_PAGE);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    setCurrentPage(1); // Сброс страницы при новом поиске
+  };
+
+  const handleSourceChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSourceFilter(e.target.value);
+    setCurrentPage(1);
+  };
 
   const handlePrevious = () => {
     setCurrentPage(prev => Math.max(prev - 1, 1));
@@ -93,24 +110,44 @@ export default function JobList({ jobs }: JobListProps) {
           // disabled={!!currentPage}
           className="px-4 py-2 bg-blue-500 text-white rounded-md ml-2"
         >
-          {_.ceil(jobs.length / JOBS_PER_PAGE)}
+          {_.ceil(filteredJobs.length / JOBS_PER_PAGE)}
         </button>
       </div>
     </div>
   )
 
   return (
-    <>
-      {getPagination()}
+      <>
+        <div className="mb-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4">
+          <input
+              type="text"
+              placeholder="Поиск"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="border border-gray-300 rounded p-2 mr-2"
+          />
+          <select
+              value={sourceFilter}
+              onChange={handleSourceChange}
+              className="border border-gray-300 rounded p-2 mr-2"
+          >
+            <option key={'olx'} value={''}>All sources</option>
+            <option key={'olx'} value={'olx.pl'}>Olx</option>
+            <option key={'jooble'} value={'jooble.pl'}>Jooble</option>
+            <option key={'bialystokonline'} value='bialystokonline.pl'>Białystok Online</option>
+          </select>
+        </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 gap-4 mt-4 mb-4">
-        {currentJobs.map(job => (
-          <JobCard key={job.id} job={job} />
-        ))}
-      </div>
-      {/*<Pagination isCompact showControls total={currentJobs.length} initialPage={1} />*/}
+        {getPagination()}
 
-      {getPagination()}
-    </>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 gap-4 mt-4 mb-4">
+          {currentJobs.map(job => (
+              <JobCard key={job.id} job={job}/>
+          ))}
+        </div>
+        {/*<Pagination isCompact showControls total={currentJobs.length} initialPage={1} />*/}
+
+        {getPagination()}
+      </>
   );
 }
